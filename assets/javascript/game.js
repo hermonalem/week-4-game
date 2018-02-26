754c4686050117a3c9f5fb74b42b42193ba70f4b
Hermon Alem <hermonalem@Hermons-MacBook-Air.local>
2018-02-26 10:35:40 -0700

    new changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+// Here we created an on-click event that responds to button clicks of the crystal image.
+$(".crystals").on("click", function(events) {
+    // Clicking the button triggers an alert message.
+    alert("You clicked a crystal!");
+
+  });
+  console.log("test");
