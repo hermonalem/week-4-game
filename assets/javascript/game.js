29cbf017a73bc0f8f2f24a60ac439a3712c911ed
Hermon Alem <hermonalem@Hermons-MacBook-Air.local>
2018-02-26 18:14:31 -0700

    latest

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,61 @@
-// Here we created an on-click event that responds to button clicks of the crystal image.
-$(".crystals").on("click", function(events) {
-    // Clicking the button triggers an alert message.
-    alert("You clicked a crystal!");
 
+var computerNumber;
+var crystal1Number;
+var crystal2Number;
+var crystal3Number;
+var crystal4Number;
+var userCount=0
+var Wins=0
+var losses=0
+
+
+$("#crystal1").on("click", function(events) {
+    userCount+=crystal1Number;
+    score();
+   console.log(crystal1Number)
   });
-  console.log("test");
+  $("#crystal2").on("click", function(events) {
+    userCount+=crystal2Number;
+    score();
+   console.log(crystal2Number)
+});
+$("#crystal3").on("click", function(events) {
+    userCount+=crystal3Number;
+    score();
+   console.log(crystal3Number)
+});
+$("#crystal4").on("click", function(events) {
+    userCount+=crystal4Number; 
+    score();
+   console.log(crystal4Number)
+});
+
+  startGame()
+  
+  //function game start//
+  function startGame () {
+      userCount=0;
+      computerNumber=Math.floor(Math.random()*101)+19;
+      crystal1Number=Math.floor(Math.random()*12)+1;
+      crystal2Number=Math.floor(Math.random()*12)+1;
+      crystal3Number=Math.floor(Math.random()*12)+1;
+      crystal4Number=Math.floor(Math.random()*12)+1;
+      $("#computernumber").html(computerNumber);
+      
+  }
+ 
+   function score(){
+       if (userCount===computerNumber){
+           Wins++
+           startGame()
+           alert("You win!")
+           $("#wins_counter").html(Wins)
+        
+       }
+       else if (userCount>computerNumber){
+           losses++
+           startGame()
+           alert("you lost!")
+           $("#losses_counter").html(losses)
+       }
+   }
